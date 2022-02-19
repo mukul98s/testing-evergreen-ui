94e3af98b88b0a0b8e5ff264da3ae09a64a02ad3
Mukul Sharma <mymukul112@gmail.com>
2022-02-19 18:21:39 +0530

    render home and schedule component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
 import Header from "./components/Header";
+import Schedule from "./page/Schedule";
 import Container from "./components/Container";
 
 function App() {
   return (
     <Container>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/meeting" element={<Schedule />} />
+      </Routes>
     </Container>
   );
 }
