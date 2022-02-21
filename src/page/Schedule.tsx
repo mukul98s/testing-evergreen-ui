3c299cf7abd2c21fb8b1b241ab543a2daf5ec0d0
Mukul Sharma <mymukul112@gmail.com>
2022-02-21 19:25:52 +0530

    add event handles and theme changing functionality

--- src/page/Schedule.tsx
+++ src/page/Schedule.tsx
@@ -2,12 +2,21 @@ import React, { useMemo, useState } from "react";
 import {
   Eventcalendar,
   MbscCalendarEvent,
+  MbscCellClickEvent,
+  MbscCellHoverEvent,
   MbscEventcalendarView,
 } from "@mobiscroll/react";
+import { Heading, majorScale, Pane, Button } from "evergreen-ui";
+
 import "@mobiscroll/react/dist/css/mobiscroll.min.css";
-import { majorScale, Pane } from "evergreen-ui";
+import { EventDialog } from "../components/EventDialog";
+
+type ThemeVariant = "dark" | "light" | "auto" | undefined;
 
 const Schedule: React.FC = () => {
+  const [theme, setTheme] = useState("ios");
+  const [themeVariant, setThemeVariant] = useState<ThemeVariant>("auto");
+  const [isShown, setIsShown] = useState(false);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [events, setEvents] = useState<MbscCalendarEvent[]>([
     {
@@ -34,6 +43,7 @@ const Schedule: React.FC = () => {
     {
       start: new Date(),
       title: "My First Event",
+      allDay: true,
     },
     {
       start: new Date(),
@@ -48,18 +58,105 @@ const Schedule: React.FC = () => {
     };
   }, []);
 
+  const onCellClick = (event: MbscCellClickEvent, inst: MbscCalendarEvent) => {
+    console.log(inst);
+  };
+
+  const onCellHoverIn = (
+    event: MbscCellHoverEvent,
+    inst: MbscCalendarEvent
+  ) => {};
+
+  const onCellHoverOut = (
+    event: MbscCellHoverEvent,
+    inst: MbscCalendarEvent
+  ) => {};
+
+  const createEvent = () => {
+    setIsShown(true);
+  };
+
   return (
-    <Pane border marginY={majorScale(2)}>
-      <Eventcalendar
-        data={events}
-        actionableEvents
-        clickToCreate
-        view={view}
-        theme="ios"
-        dragToMove
-        dragToCreate
-      />
-    </Pane>
+    <>
+      <Pane border marginY={majorScale(2)}>
+        <Eventcalendar
+          data={events}
+          actionableEvents
+          clickToCreate
+          view={view}
+          theme={theme}
+          themeVariant={themeVariant}
+          dragToMove
+          dragToCreate
+          onCellClick={onCellClick}
+          onCellHoverIn={onCellHoverIn}
+          onCellHoverOut={onCellHoverOut}
+        />
+      </Pane>
+      <Pane
+        border
+        elevation={2}
+        margin={majorScale(2)}
+        padding={majorScale(2)}
+        display="flex"
+        flexDirection="column"
+        gap={majorScale(3)}
+      >
+        <Heading>Select Calender Theme</Heading>
+        <Pane display="flex" gap={majorScale(2)}>
+          <Button
+            appearance="primary"
+            intent="danger"
+            onClick={() => setTheme("ios")}
+          >
+            iOS
+          </Button>
+          <Button
+            appearance="primary"
+            intent="danger"
+            onClick={() => setTheme("windows")}
+          >
+            Windows
+          </Button>
+          <Button
+            appearance="primary"
+            intent="danger"
+            onClick={() => setTheme("material")}
+          >
+            Material
+          </Button>
+        </Pane>
+
+        <Heading>Select Color Theme </Heading>
+        <Pane display="flex" gap={majorScale(2)}>
+          <Button
+            appearance="primary"
+            intent="danger"
+            onClick={() => setThemeVariant("dark")}
+          >
+            Dark
+          </Button>
+          <Button
+            appearance="primary"
+            intent="danger"
+            onClick={() => setThemeVariant("light")}
+          >
+            Light
+          </Button>
+        </Pane>
+
+        <Heading>CRUD Functions</Heading>
+        <Pane display="flex" gap={majorScale(2)}>
+          <EventDialog isShown={isShown} setIsShown={setIsShown} />
+          <Button appearance="primary" intent="danger" onClick={createEvent}>
+            Create Event
+          </Button>
+          <Button appearance="primary" intent="danger"></Button>
+          <Button appearance="primary" intent="danger"></Button>
+          <Button appearance="primary" intent="danger"></Button>
+        </Pane>
+      </Pane>
+    </>
   );
 };
 
